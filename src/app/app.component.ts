6cf10d5bd997aca0d441ea1ed1e22e310a4a5bf0
Alessandro <als.bencini@gmail.com>
2022-07-03 20:57:49 +0200

    Added GDPR, new bio and youtube embed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'tarsia';
+
+  cookieMessage =
+    'Questo sito o gli strumenti terzi da questo utilizzati si avvalgono di cookie necessari al funzionamento ed utili alle finalità illustrate nella cookie policy.';
+  cookieDismiss = 'Accetta';
+  cookieLinkText = 'Scopri di più';
+
+  ngOnInit() {
+    let cc = window as any;
+    cc.cookieconsent.initialise({
+      /*  palette: {
+        popup: {
+          background: '#164969',
+        },
+        button: {
+          background: '#ffe000',
+          text: '#164969',
+        },
+      }, */
+      theme: 'classic',
+      content: {
+        message: this.cookieMessage,
+        dismiss: this.cookieDismiss,
+        link: this.cookieLinkText,
+        /* href: environment.Frontend + '/dataprivacy', */
+      },
+    });
+  }
 }
